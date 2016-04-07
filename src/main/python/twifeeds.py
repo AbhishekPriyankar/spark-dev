feec7519bcf4413f7ce98481c2207fbfe9fa73d1
prithvirajbose <prithvirajbose@hotmail.com>
2016-04-07 19:54:21 +0530

    made twifeeds.py as kafka producer

--- src/main/python/twifeeds.py
+++ src/main/python/twifeeds.py
@@ -1,7 +1,12 @@
 
 from tweepy import Stream, OAuthHandler, StreamListener, TweepError
+from kafka import KafkaProducer #Tested on kafka-python 1.0.2
 import json
 import sys
+
+"""
+Can be tested with kafka-console-consumer.sh.
+"""
 
 def load_auth_info(keys_file, app_name):
 
@@ -14,31 +19,38 @@ def load_auth_info(keys_file, app_name):
 class TwitterStreamListener(StreamListener):
 
     def on_data(self, data):
-        d = json.loads(data)
-		# send to kafka topic
+        # TODO: remove topic name hardcoding...
+        producer.send('test', json.loads(data))        
         return True
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 4:
-        print '>>> Usage: python collector.py twitter_app_keys_file twitter_app_name'
+    if len(sys.argv) != 3:
+        print '>>> Usage: python twifeeds.py twitter_app_keys_file twitter_app_name'
         sys.exit(-1)
 
     keys = load_auth_info(sys.argv[1], sys.argv[2])
 
     try:
+        producer = KafkaProducer(bootstrap_servers=['localhost:9092'], \
+                                 value_serializer=lambda d: json.dumps(d).encode('utf-8'))
+        
         auth = OAuthHandler(keys['api_key'], keys['api_secret'])
         auth.set_access_token(keys['access_token'], keys['access_token_secret'])
 
         stream = Stream(auth = auth, listener = TwitterStreamListener())
+        # TODO: Remove filter hardcoding...
         stream.filter(track = ['global warming', 'oil spill'])
         
     except TweepError as e:
         print '>>> tweepy error: ', e
     except KeyboardInterrupt:
         print '>>> Keyboard interrupt received...'
+    except Exception as e:
+        print '>>> kafka/other errors: ', e
     finally:
         print '>>> Closing twitter stream...'
         stream.disconnect()
+        #producer.close()
 
 
